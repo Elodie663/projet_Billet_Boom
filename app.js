6d7c6ba3a774c15c2a5f145e42eae5d6ab120b4a
Elodie1405 <elodie.molieres14@gmail.com>
2025-10-25 16:08:53 +0200

    Feature: theatre testé et fonctionnel

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/database.js";
 import cinemaRoute from "./routes/cinemaRoute.js";
+import theatreRoute from "./routes/theatreRoute.js";
 // Charger les variables d'environnement
 dotenv.config();
 
@@ -16,6 +17,9 @@ connectDB(); //
 
 //route principale pour la page cinema
 app.use("/cinema", cinemaRoute);
+
+//route principale pour la page théâtre
+app.use("/theatre", theatreRoute);
 
 // Démarrer le serveur
 app.listen(PORT, () => {
